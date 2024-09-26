8add9b68b4de086e1835da2b08b09d36a09a6909
Frédéric Oudenot <f.oudenot@gmail.com>
2024-09-26 19:45:15 +0200

    update: conditions

--- JS/conditions/index.js
+++ JS/conditions/index.js
@@ -1,19 +1,13 @@
 // Declare and define three variables using a numerical value
-const a = 0;
-const b = 3;
-const c = 1;
+const number1 = 0;
+const number2 = 3;
+const number3 = 1;
 
 // * If the three variables are equal, display "The three variables are the same" in the console
-if (a === b && b === c && a === c) {
-  console.log("The three variables are the same");
-}
-
-// * If only two of the variables are equal, display "Two of the variables are the same" in the console
-if (a === b || b === c || a === c) {
-  console.log("Two of the variables are the same");
-}
-
-// * If the variables are all different display "all variables are different" in the console
-if (a !== b && b !== c && a !== c) {
-	console.log("The three variables are the same");
+if(number1 === number2 && number2 ===  number3){
+  console.log("les trois variables sont identiques");
+}else if(number1 !== number2 || number2 !== number3){
+  console.log("Deux variables sont egales");
+}else{
+console.log("Toutes variables sont differentes");
 }
