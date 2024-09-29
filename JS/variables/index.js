759290da119eaab7396b66b4eddedde35c809c67
Frédéric Oudenot <f.oudenot@gmail.com>
2024-09-29 15:46:54 +0200

    build: pratice variables and keywords

---
+++ JS/variables/index.js
@@ -0,0 +1,28 @@
+
+  /*
+    🐦 DAVID Bruno Twitter profile 🐦
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+    const firstname = "David"; // ✅ Good!  
+    const lastname = "Bruno"; // ❌ the keyword to declare the variable is missing 
+  
+    
+    let biographyMessage = `Hi, I'm ${firstname} ${lastname} from SF, I like to cook and meet new people.`; // ❌ the variable name is not explicit. (this is David's biography)
+  
+  
+    const profilePicture = "http://www.go.com/davif.png"; // ❌ the variable name is not explicit and the keyword is missing (this is David's profile picture)
+  
+    const city = "San Francisco"; // ❌ the variable name is not explicit and the keyword is missing (try to guess what "San Francisco" could refer to)
+  
+    let followers = 109; // ❌ the keyword to declare the variable is missing 
+    let following = 200; // ❌ the keyword to declare the variable is missing 
+  
+  // 🏁 Exercise 02 - David is following one more account increment the total of following account
+  
+following++;
+console.log(following);
