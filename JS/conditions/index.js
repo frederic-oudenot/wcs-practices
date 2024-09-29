56182adba1003b3eda824fc35ae8e6f8e960be62
Frédéric Oudenot <f.oudenot@gmail.com>
2024-09-29 12:02:48 +0200

    fix: error conditions two variables are identical

--- JS/conditions/index.js
+++ JS/conditions/index.js
@@ -1,13 +1,18 @@
 // Declare and define three variables using a numerical value
-const number1 = 0;
+const number1 = 2;
 const number2 = 3;
 const number3 = 1;
 
-// * If the three variables are equal, display "The three variables are the same" in the console
-if(number1 === number2 && number2 ===  number3){
-  console.log("les trois variables sont identiques");
-}else if(number1 !== number2 || number2 !== number3){
-  console.log("Deux variables sont egales");
-}else{
-console.log("Toutes variables sont differentes");
+// Si les trois variables sont égales, affiche "Les trois variables sont identiques" dans la console
+if (number1 === number2 && number2 === number3) {
+	console.log("les trois variables sont identiques");
+}
+
+// Si seulement deux des variables sont égales, affiche "Deux des variables sont identiques" dans la console
+else if (number1 === number2 && number2 !== number3 || number2 === number3 && number1 !== number3 || number1 === number3 && number1 !== number2) {
+	console.log("Deux variables sont egales");
+}
+// Si les variables sont toutes différentes, affiche "toutes les variables sont différentes" dans la console
+else {
+	console.log("Toutes variables sont differentes");
 }
